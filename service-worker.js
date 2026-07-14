f02822d903aad6b9e413e16af88d1903875136bb
Anguelion <128149734+Anguelion@users.noreply.github.com>
2026-07-14 20:02:52 -0300

    Reconstrói questões e gabaritos do bloco 01

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = 'soqueromed-shell-v69';
+const CACHE_NAME = 'soqueromed-shell-v70';
 const APP_SHELL = [
   './',
   './manifest.webmanifest',
   './assets/planner.css?v=20260714-35',
-  './assets/planner.js?v=20260714-50',
+  './assets/planner.js?v=20260714-51',
   './assets/icons/icon-192.png',
   './assets/icons/icon-512.png',
   './assets/icons/icon-512-maskable.png',
