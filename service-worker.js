1c01c6c2ceeca38e4dae1c01860dda0e3bde2f2a
Anguelion <128149734+Anguelion@users.noreply.github.com>
2026-07-14 14:18:03 -0300

    Compacta trilha diaria em colunas

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = 'soqueromed-shell-v58';
+const CACHE_NAME = 'soqueromed-shell-v59';
 const APP_SHELL = [
   './',
   './manifest.webmanifest',
-  './assets/planner.css?v=20260714-29',
-  './assets/planner.js?v=20260714-43',
+  './assets/planner.css?v=20260714-30',
+  './assets/planner.js?v=20260714-44',
   './assets/icons/icon-192.png',
   './assets/icons/icon-512.png',
   './assets/icons/icon-512-maskable.png',
