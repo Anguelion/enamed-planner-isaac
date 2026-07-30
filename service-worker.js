38363c2665dc3aa0865235142d12be858ea02154
Anguelion <128149734+Anguelion@users.noreply.github.com>
2026-07-30 11:00:18 -0300

    fix: soma revisoes automaticas de flashcards ao total exibido no cronograma, igual questoes

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'soqueromed-shell-v157';
+const CACHE_NAME = 'soqueromed-shell-v158';
 const APP_SHELL = [
   './',
   './manifest.webmanifest',
@@ -12,7 +12,7 @@ const APP_SHELL = [
   './assets/semiologia-aulas.js?v=20260720-2',
   './assets/semiologia.js?v=20260720-7',
   './assets/caso-do-dia.js?v=20260729-4',
-  './assets/planner.js?v=20260730-10',
+  './assets/planner.js?v=20260730-11',
   './assets/icons/icon-192.png',
   './assets/icons/icon-512.png',
   './assets/icons/icon-512-maskable.png',
