44a9b60cc4b1f5e01b3ffb7013085d91d08c70c4
Anguelion <128149734+Anguelion@users.noreply.github.com>
2026-07-30 11:07:42 -0300

    feat: adiciona botao explicito de Receber da nuvem ao lado de Enviar em Ferramentas

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'soqueromed-shell-v158';
+const CACHE_NAME = 'soqueromed-shell-v159';
 const APP_SHELL = [
   './',
   './manifest.webmanifest',
@@ -12,7 +12,7 @@ const APP_SHELL = [
   './assets/semiologia-aulas.js?v=20260720-2',
   './assets/semiologia.js?v=20260720-7',
   './assets/caso-do-dia.js?v=20260729-4',
-  './assets/planner.js?v=20260730-11',
+  './assets/planner.js?v=20260730-12',
   './assets/icons/icon-192.png',
   './assets/icons/icon-512.png',
   './assets/icons/icon-512-maskable.png',
